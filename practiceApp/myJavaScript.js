81b1c748805c88b5ee6a3b40ccf98f9fee7432e1
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-14 16:33:01 -0700

    added the value input check function

--- practiceApp/myJavaScript.js
+++ practiceApp/myJavaScript.js
@@ -4,3 +4,17 @@ document.getElementById("demo").innerHTML="Hi into JavaScript! "*/
 
 /* using query selector */
 document.querySelector("#demo").innerHTML="Hi again!"
+var but=document.getElementById("buttonOne");
+var number=document.querySelector("#num");
+but.addEventListener('click', myFunction);
+function myFunction(){
+	var text;
+	if(0<=number.value<=10){
+text="entered value ok!";
+
+	}
+	else {
+		text="wrong value entered. Please try again!";
+	}
+	document.querySelector("#demo").innerHTML=number.value;
+}
