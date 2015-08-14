0201f63f2355b90450fca50401976117abd87ca5
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-14 11:54:07 -0700

    added web project for practice

---
+++ practiceApp/myJavaScript.js
@@ -0,0 +1,6 @@
+/*using getElementId first 
+
+document.getElementById("demo").innerHTML="Hi into JavaScript! "*/
+
+/* using query selector */
+document.querySelector("#demo").innerHTML="Hi again!"
